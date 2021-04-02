01c803a1039ee647f97a614dab515fb120d7c74b
Cassie Nutter <cassie.m.nutter@gmail.com>
2021-04-02 10:03:55 -0500

    add gitignore items

---
+++ config.py
@@ -0,0 +1,5 @@
+# Developer API keys
+consumer_key = 'bWejmjpTvWNkCiwpixz4iYxGK'
+consumer_secret = 'Yambe1a5bWbsPoyKQg8MeQzdVHlcjnXzIm6k7uGDPsKvxyDki7'
+access_token = '1374027292867969025-BcTPabYKmlcKQHZCkoXbZ2BvXXcbNs'
+access_token_secret = 'STtqBirnsLcjoVkkRbtIpWLb55hTtyw6dJus8kwaxaqha'
